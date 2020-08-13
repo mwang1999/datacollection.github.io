f69c456bf3e91a16154d77589e7918d37dd039b1
mwang1999 <31618826+mwang1999@users.noreply.github.com>
2020-08-13 12:37:54 -0700

    Installed Express and Web Server Cookies

---
+++ index.js
@@ -0,0 +1,12 @@
+const app = require("express")()
+app.get("/", (req, res) => {
+    res.setHeader("set-cookie", ["setfromserver=1", "jscantseethis =1; httponly"]) //Web server cookie
+    res.sendfile('${__dirname}/index.html') //Client set cookie
+
+    app.get("/path1", (req, res) => {
+        
+        res.send('Sent cookies:${req.headers.cookie}')
+    })
+
+})
+
