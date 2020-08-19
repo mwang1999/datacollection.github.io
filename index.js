99824a8818a02b999118037f77915cd8b7e3b464
mwang1999 <31618826+mwang1999@users.noreply.github.com>
2020-08-19 15:51:51 -0700

    web sever

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("express")()
 app.get("/", (req, res) => {
-    res.setHeader("set-cookie", ["setfromserver=1", "jscantseethis =1; httponly"]) //Web server cookie
+    
+    res.setHeader('set-cookie', ["setfromserver=1", "jscantseethis =1; httponly"]) //Web server cookie
     res.sendfile('${__dirname}/index.html') //Client set cookie
 
     app.get("/path1", (req, res) => {
@@ -9,4 +10,6 @@ app.get("/", (req, res) => {
     })
 
 })
+
+app.listen(8080, () => console.log("listening on port 8080"))
 
