7562bd6e3b849166980601a5fefe92d1cf3db80b
mwang1999 <31618826+mwang1999@users.noreply.github.com>
2020-09-29 17:38:48 -0700

    Changed server.js

--- server.js
+++ server.js
@@ -1,47 +1,6 @@
-/*
-var http = require('http');
-var fs = require('fs');
-var PORT = 5000; 
-
-const server = http.createServer(function(req,res){
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    fs.readFile('index.html', function(error, data)
-    {
-        if (error){
-            res.writeHead(404)
-            res.write('Error: File Not Found')
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
-    res.end()
-})
-
-server.listen(port, function(error){
-    if (error) {
-        console.log("Error", error)
-    } else {
-        console.log("Server is listening on port" + port)
-    }
-})
-*/
-
-/*
-var express = require('express');
-var cookieParser = require ('cookie-parser');
-
-var app = express();
-app.use(cookieParser());
-
-app.get('/'. function(req, resp) { 
-    resp.cookie('IPCookie', 'response', {maxAge: 6000 seconds, httpOnly: true, secure: process.env.NODE_ENV === 'production'? true: false});
-});
-*/
-
 const http = require('http')
 const fs = require('fs')
-const port = 3000
+var port = process.env.PORT || 3000;
 
 const server = http.createServer(function(req,res){
     res.writeHead(200, { 'Content-Type': 'text/html' })
@@ -63,5 +22,7 @@ server.listen(port, function(error){
         console.log('Server is listening on port' + port)
     }
 })
+
+
 
 
